54032c98efed3fa260ed9f291b76553ba22d8ae4
Leandro.Battisti <leandro.battisti@senior.com.br>
2016-01-19 11:15:29 -0200

    rotas funcionando.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,15 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 
 Route::post('/task', function (Request $request) {
@@ -49,22 +56,39 @@ Route::group(['middleware' => ['web']], function () {
     /**
      * Show Task Dashboard
      */
-    Route::get('/', function () {
-        //
-        return view('tasks');
+     Route::get('/', function () {
+        return view('tasks', [
+        	'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
     });
+
 
     /**
      * Add New Task
      */
-    Route::post('/task', function (Request $request) {
-        //
-    });
+     Route::post('/task', function (Request $request) {
+         $validator = Validator::make($request->all(), [
+             'name' => 'required|max:255',
+         ]);
 
+         if ($validator->fails()) {
+             return redirect('/')
+                 ->withInput()
+                 ->withErrors($validator);
+         }
+
+         $task = new Task;
+         $task->name = $request->name;
+         $task->save();
+
+         return redirect('/');
+     });
     /**
      * Delete Task
      */
-    Route::delete('/task/{task}', function (Task $task) {
-        //
-    });
+     Route::delete('/task/{task}', function (Task $task) {
+         $task->delete();
+
+         return redirect('/');
+     });
 });
