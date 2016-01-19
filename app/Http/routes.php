7650b04532720b9ccffcd60449f285de38409c68
Leandro.Battisti <leandro.battisti@senior.com.br>
2016-01-19 16:05:26 -0200

    iniciado o nível intermediário do quickstart

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,32 +14,6 @@
 use App\Task;
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
-
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
